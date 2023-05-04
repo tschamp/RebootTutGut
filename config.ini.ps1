9ad700ec36fd497bc286b5417652506820fe1341
davisko5 <108990357+davisko5@users.noreply.github.com>
2023-05-04 10:16:46 +0200

    Update config.ini.ps1

--- config.ini.ps1
+++ config.ini.ps1
@@ -1,3 +1,21 @@
-## Halli Hallo das isch die Config wege Pfade und Made und soo
+#--------------------------------------------------------------------------------
+# Autor: David Strainovic & Timo Schreiber
+# Funktion des Skripts: Eine interne Config File, für Pfäde und konstanten
+# Erstellungsdatum: 04.05.2023
+# Version: 1.0
+# Bemerkungen: -
+#--------------------------------------------------------------------------------
 
-$test = C:\victor
+
+
+# HashTable der Variable $config mit den Keys und Values.
+$config = @{
+    SchuelerCsv  = "C:\tmp\PSProjekt\schueler.csv"
+    InitPw       = "bztf.001"
+    OUPath       = "OU=BZTF,DC=bztf,DC=local"
+    OULernende   = "Lernende"
+    OUKlasse     = "Klassengruppen"
+    LogFileUser  = "C:\tmp\users.log"
+    LogFileGroup = "C:\tmp\groups.log"
+    ClassFolder  = "C:\BZTF\Klassen"
+}
