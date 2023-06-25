5c9d275602818967121144de0a34eae34c3fcd65
Timo Schreiber <123559411+tschamp@users.noreply.github.com>
2023-06-25 13:53:19 +0200

    Update a_AD_User_erstellen.ps1

--- src/1/a_AD_User_erstellen.ps1
+++ src/1/a_AD_User_erstellen.ps1
@@ -1,34 +1,52 @@
-# Pfad zur XML-Datei
-$xmlFilePath = "c.\users\timo.schreiber\documents\files\schueler.xml"
+# Pfade zur XML-Datei und zum Active Directory-Modul
+$xmlFilePath = "C:\Users\timo.schreiber\Documents\GitHub\RebootTutGut\files\schueler.xml"
 
-# XML-Datei lesen
-$xmlData = Get-Content -Path $xmlFilePath
-
-# XML-Daten analysieren und Benutzer erstellen
-$xmlUsers = [xml]$xmlData
-
-# Active Directory-Modul importieren
+# Importieren des Active Directory-Moduls
 Import-Module ActiveDirectory
 
-foreach ($userNode in $xmlUsers.S) {
-    $userAttributes = $userNode.InnerText -split ";"
+# Funktion zum Erstellen eines AD-Benutzerkontos
+function Create-ADUser {
+    param(
+        [string]$Name,
+        [string]$Vorname,
+        [string]$Benutzername,
+        [string]$Klasse,
+        [string]$Klasse2
+    )
+    
+    # Benutzerattribute festlegen
+    $SamAccountName = $Benutzername
+    $UserPrincipalName = "$SamAccountName@bztf.ch"
+    $Password = ConvertTo-SecureString -String "Passwort123" -AsPlainText -Force
+    
+    # Benutzerobjekt erstellen und dem AD hinzufügen
+    $userParams = @{
+        SamAccountName = $SamAccountName
+        UserPrincipalName = $UserPrincipalName
+        Name = "$Vorname $Name"
+        GivenName = $Vorname
+        Surname = $Name
+        Enabled = $true
+        PasswordNeverExpires = $true
+        AccountPassword = $Password
+        Path = "CN=Schueler,DC=reboottutgut,DC=local"
+    }
+    New-ADUser @userParams
+    
+    # Klasse zuweisen
+    $user = Get-ADUser -Filter "SamAccountName -eq '$SamAccountName'"
+    Set-ADUser -Identity $user -Description "$Klasse, $Klasse2"
+}
 
-    $lastName = $userAttributes[0]
-    $firstName = $userAttributes[1]
-    $username = $userAttributes[2]
-    $class = $userAttributes[3]
+# XML-Datei einlesen und Benutzer erstellen
+$xml = [xml](Get-Content $xmlFilePath)
 
-    # Passwort für den Benutzer generieren
-    $password = ConvertTo-SecureString -String "Passwort123" -AsPlainText -Force
-
-    # Benutzerkonto erstellen
-    New-ADUser -SamAccountName $username `
-               -GivenName $firstName `
-               -Surname $lastName `
-               -Name "$firstName $lastName" `
-               -DisplayName "$lastName, $firstName" `
-               -Description "Klasse: $class," `
-               -Enabled $true `
-               -AccountPassword $password
-               -Path = "OU=schueler,DC=reboottutgut,DC=local"
+foreach ($userNode in $xml.MS.S) {
+    $name = $userNode."#Name"
+    $vorname = $userNode."#Vorname"
+    $benutzername = $userNode."#Benutzername"
+    $klasse = $userNode."#Klasse"
+    $klasse2 = $userNode."#Klasse2"
+    
+    Create-ADUser -Name $name -Vorname $vorname -Benutzername $benutzername -Klasse $klasse -Klasse2 $klasse2
 }
